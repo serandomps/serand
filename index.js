f5ae3ce0476c3c446d3ac760cd642e6b54944999
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-04-17 00:52:31 +0530

    sso changes

--- index.js
+++ index.js
@@ -136,3 +136,27 @@ $(function () {
         e.preventDefault();
     });
 });
+
+module.exports.cache = function (key, val) {
+    if (val) {
+        sessionStorage.setItem(key, JSON.stringify(val));
+        return val;
+    }
+    if (val === null) {
+        return sessionStorage.removeItem(key);
+    }
+    val = sessionStorage.getItem(key);
+    return val ? JSON.parse(val) : null;
+};
+
+module.exports.store = function (key, val) {
+    if (val) {
+        localStorage.setItem(key, JSON.stringify(val));
+        return val;
+    }
+    var o = localStorage.getItem(key);
+    if (val === null) {
+        localStorage.removeItem(key);
+    }
+    return o ? JSON.parse(o) : null;
+};
